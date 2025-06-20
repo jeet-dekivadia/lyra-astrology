796b55e403e8cdcfb04c7c97dac0d7a5924b1213
Jeet Dekivadia <jeet.university@gmail.com>
2025-06-20 14:37:07 +0530

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = '';
-const supabaseAnonKey = '';
+const supabaseUrl = 'https://qlgodilqctrgejpjvpqc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsZ29kaWxxY3RyZ2VqcGp2cHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxNzU2NzAsImV4cCI6MjA2NTc1MTY3MH0.0ouQeEIng_5cYPN9JmMU_W2ycFH0gYREzLW1-jPC8ZM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
